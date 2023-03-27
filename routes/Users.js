62ddc26cc2af480a5523f143249f1420ab6bb6d2
Melanie Kuhn <melanie@jebbit.com>
2023-03-27 13:21:05 -0400

    initial commit

---
+++ routes/Users.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+let User = require('../models/User.js');
+
+router.route('/add').post((req, res) => {
+    const { username, score } = req.body;
+    const newUser = new User({username, score});
+
+    newUser.save()
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error ' + err));
+});
+
+module.exports = router;
